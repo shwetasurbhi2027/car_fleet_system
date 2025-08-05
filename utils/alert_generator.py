44a4a242249a8d2192666fed503d8bc199a992b3
shwetasurbhi2027 <144616595+shwetasurbhi2027@users.noreply.github.com>
2025-08-05 16:45:19 +0530

    Update alert_generator.py

--- utils/alert_generator.py
+++ utils/alert_generator.py
@@ -1,14 +1,42 @@
 import uuid
-from datetime import datetime
 from models.alert import Alert
-from storage.database import alerts
+from storage.database import alerts_db
+from datetime import datetime
 
-def generate_alert(vin, type_, severity):
-    alert = Alert(
-        alert_id=str(uuid.uuid4()),
-        vin=vin,
-        type=type_,
-        severity=severity,
-        timestamp=datetime.utcnow()
-    )
-    alerts[alert.alert_id] = alert
+def generate_alerts(data):
+    if data.speed > 120:
+        alerts_db.append(Alert(
+            vehicle_id=data.vehicle_id,
+            alert_type="Speed Violation",
+            severity="High",
+            message=f"Speed {data.speed} km/h",
+            timestamp=data.timestamp
+        ))
+
+    if data.fuel_level is not None and data.fuel_level < 15:
+        alerts_db.append(Alert(
+            vehicle_id=data.vehicle_id,
+            alert_type="Low Fuel",
+            severity="Medium",
+            message=f"Fuel Level: {data.fuel_level}%",
+            timestamp=data.timestamp
+        ))
+
+    if data.battery_level is not None and data.battery_level < 15:
+        alerts_db.append(Alert(
+            vehicle_id=data.vehicle_id,
+            alert_type="Low Battery",
+            severity="Medium",
+            message=f"Battery Level: {data.battery_level}%",
+            timestamp=data.timestamp
+        ))
+
+    if data.diagnostic_code:
+        alerts_db.append(Alert(
+            vehicle_id=data.vehicle_id,
+            alert_type="Diagnostic Code",
+            severity="Critical",
+            message=f"Code: {data.diagnostic_code}",
+            timestamp=data.timestamp
+        ))
+
