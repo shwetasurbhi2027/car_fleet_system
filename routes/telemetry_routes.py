e3c1baabddb9e325887a9f349dc01f50a422209b
shwetasurbhi2027 <144616595+shwetasurbhi2027@users.noreply.github.com>
2025-08-05 15:10:02 +0530

    Update telemetry_routes.py

--- routes/telemetry_routes.py
+++ routes/telemetry_routes.py
@@ -1,29 +1,25 @@
 from fastapi import APIRouter, HTTPException
 from models.telemetry import Telemetry
-from storage.database import telemetry_latest, telemetry_history, vehicles
-from utils.alert_generator import generate_alert
+from services.telemetry_service import (
+    receive_telemetry_service,
+    get_latest_telemetry_service
+)
 
 router = APIRouter()
 
 @router.post("/")
 def receive_telemetry(data: Telemetry):
-    if data.vin not in vehicles:
+    success = receive_telemetry_service(data)
+    if not success:
         raise HTTPException(status_code=404, detail="Vehicle not registered")
-    
-    telemetry_latest[data.vin] = data
-    telemetry_history.setdefault(data.vin, []).append(data)
-
-    # Alerts
-    if data.speed > 120:
-        generate_alert(data.vin, "Speed Violation", "High")
-    if data.fuel_level < 15:
-        generate_alert(data.vin, "Low Fuel", "Low")
-
     return {"msg": "Telemetry received"}
 
 @router.get("/{vin}")
 def get_latest_telemetry(vin: str):
-    if vin not in telemetry_latest:
-        raise HTTPException(status_code=404, detail="No data found")
-    return telemetry_latest[vin]
+    data = get_latest_telemetry_service(vin)
+    if not data:
+        raise HTTPException(status_code=404, detail="No telemetry found")
+    return data
+
+
 
