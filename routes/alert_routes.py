09826e4386ed960f1fdef7d6c0b10829e8e99a23
shwetasurbhi2027 <144616595+shwetasurbhi2027@users.noreply.github.com>
2025-08-05 15:05:59 +0530

    Update alert_routes.py

--- routes/alert_routes.py
+++ routes/alert_routes.py
@@ -1,1 +1,14 @@
+Router()
 
+@router.get("/")
+def get_all_alerts():
+    return list(alerts.values())
+
+@router.get("/{alert_idfrom fastapi import APIRouter, HTTPException
+from storage.database import alerts
+
+router = API}")
+def get_alert(alert_id: str):
+    if alert_id in alerts:
+        return alerts[alert_id]
+    raise HTTPException(status_code=404, detail="Alert not found")
