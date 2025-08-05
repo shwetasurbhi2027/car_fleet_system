ee2af4e475400eeb7b7e238bf6deda4224231348
shwetasurbhi2027 <144616595+shwetasurbhi2027@users.noreply.github.com>
2025-08-05 14:37:58 +0530

    Update alert.py

--- models/alert.py
+++ models/alert.py
@@ -1,1 +1,9 @@
+from pydantic import BaseModel
+from datetime import datetime
 
+class Alert(BaseModel):
+    alert_id: str
+    vin: str
+    type: str  # Speed Violation, Low Fuel/Battery
+    severity: str  # Low, High
+    timestamp: datetime
