a3d2e49424ba60d9ba8902bd57f7cf2b9560ec21
shwetasurbhi2027 <144616595+shwetasurbhi2027@users.noreply.github.com>
2025-08-05 15:08:13 +0530

    Update vehicle_services.py

--- services/vehicle_services.py
+++ services/vehicle_services.py
@@ -1,1 +1,18 @@
+from models.vehicle import Vehicle
+from storage.database import vehicles
+
+def add_vehicle_service(vehicle: Vehicle):
+    if vehicle.vin in vehicles:
+        return False
+    vehicles[vehicle.vin] = vehicle
+    return True
+
+def list_vehicles_service():
+    return list(vehicles.values())
+
+def get_vehicle_service(vin: str):
+    return vehicles.get(vin)
+
+def delete_vehicle_service(vin: str):
+    return vehicles.pop(vin, None)
 
