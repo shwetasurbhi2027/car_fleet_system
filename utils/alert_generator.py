3498329f125bef0db0b698a7469fe60537a9233e
shwetasurbhi2027 <144616595+shwetasurbhi2027@users.noreply.github.com>
2025-08-05 14:53:28 +0530

    Update alert_generator.py

--- utils/alert_generator.py
+++ utils/alert_generator.py
@@ -1,1 +1,14 @@
+import uuid
+from datetime import datetime
+from models.alert import Alert
+from storage.database import alerts
 
+def generate_alert(vin, type_, severity):
+    alert = Alert(
+        alert_id=str(uuid.uuid4()),
+        vin=vin,
+        type=type_,
+        severity=severity,
+        timestamp=datetime.utcnow()
+    )
+    alerts[alert.alert_id] = alert
